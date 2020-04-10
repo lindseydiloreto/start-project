c7dcb4b73d655eb6707a46d535b6830ee9445ec6
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2020-04-10 00:09:11 -0700

    Partial Redactor implementation

---
+++ config/colour-swatches.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'colours' => [
+        [
+            'label' => 'White',
+            'color' => '#fff',
+            'default' => true,
+        ],        [
+            'label' => 'Grey',
+            'color' => '#f5f5f7',
+            'default' => false,
+        ],
+        [
+            'label' => 'Blue 1',
+            'color' => '#23325a',
+            'default' => false,
+        ],
+        [
+            'label' => 'Blue 2',
+            'color' => '#25418c',
+            'default' => false,
+        ],
+        [
+            'label' => 'Blue 3',
+            'color' => '#03508b',
+            'default' => false,
+        ],
+        [
+            'label' => 'Blue 4',
+            'color' => '#213c60',
+            'default' => false,
+        ],
+        [
+            'label' => 'Blue 5',
+            'color' => '#0193de',
+            'default' => false,
+        ],
+    ],
+];

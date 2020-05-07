085d57d5e18267ac273bb7d1f5fdf6a4b088e893
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2020-05-07 13:39:59 -0700

    Organized config

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'server' => '127.0.0.1',
-    'user' => getenv('DB_USER'),
+    'server'   => '127.0.0.1',
+    'user'     => getenv('DB_USER'),
     'password' => getenv('DB_PASSWORD'),
     'database' => getenv('DB_DATABASE'),
 ];
--- config/general.php
+++ config/general.php
@@ -3,36 +3,43 @@
 return [
     // Global settings
     '*' => [
+        'cpTrigger'   => 'cp',
+        'siteUrl'     => getenv('CONFIG_SITEURL'),
         'securityKey' => getenv('SECURITY_KEY'),
-        'siteUrl' => getenv('CONFIG_SITEURL'),
-        'cpTrigger' => 'cp',
+
+        'useEmailAsUsername'   => true,
         'omitScriptNameInUrls' => true,
-        'allowUpdates' => false,
-        'sendPoweredByHeader' => false,
-        'cacheDuration' => 2592000, // Cache for 30 days
-        'userSessionDuration' => 'P1D',
-        'rememberedUserSessionDuration' => 'P1Y',
-        'verificationCodeDuration' => 'P2W',
-        'useEmailAsUsername' => true,
-        'defaultSearchTermOptions' => ['subLeft' => true],
+        'allowUpdates'         => false,
+
+        'limitAutoSlugsToAscii'   => true,
         'convertFilenamesToAscii' => true,
-        'limitAutoSlugsToAscii' => true,
+
+        'cacheDuration' => 'P30D', // Cache for 30 days
+
+        'userSessionDuration'           => 'P1D', // Stay logged in for 1 day
+        'rememberedUserSessionDuration' => 'P1Y', // Stay logged in for 1 year ("Remember Me")
+        'verificationCodeDuration'      => 'P2W', // Verification codes expire after 2 weeks
+
+        'softDeleteDuration' => 'P6M', // Keep soft-deleted items for 6 months
+
+        'defaultSearchTermOptions' => ['subLeft' => true],
+
         'aliases' => [
-            '@web' => null,
-            '@webroot' => null,
-            '@baseUrl' => getenv('CONFIG_SITEURL'),
-            '@basePath' => getenv('CONFIG_SITEPATH'),
-            '@assetsUrl' => getenv('CONFIG_SITEURL').'/assets',
-            '@assetsPath' => getenv('CONFIG_SITEPATH').'/assets',
+            '@web'          => null,
+            '@webroot'      => null,
+            '@baseUrl'      => getenv('CONFIG_SITEURL'),
+            '@basePath'     => getenv('CONFIG_SITEPATH'),
+            '@assetsUrl'    => getenv('CONFIG_SITEURL').'/assets',
+            '@assetsPath'   => getenv('CONFIG_SITEPATH').'/assets',
             '@resourcesUrl' => getenv('CONFIG_SITEURL').'/resources',
         ],
     ],
     // Dev environment settings
     'dev' => [
-        'devMode' => true,
-        'allowUpdates' => true,
+        'devMode'               => true,
+        'allowUpdates'          => true,
         'enableTemplateCaching' => false,
-        'testToEmailAddress' => getenv('CONFIG_TESTTOEMAILADDRESS'),
+        'testToEmailAddress'    => getenv('CONFIG_TESTTOEMAILADDRESS'),
     ],
     // Staging environment settings
     'staging' => [

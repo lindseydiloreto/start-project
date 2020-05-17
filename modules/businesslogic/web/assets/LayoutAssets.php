1714bae4707d45b65d3e943cc982bfc89e9dfc7e
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2020-05-16 21:44:21 -0700

    Removed references to Mix plugin

--- modules/businesslogic/web/assets/LayoutAssets.php
+++ modules/businesslogic/web/assets/LayoutAssets.php
@@ -2,7 +2,6 @@
 namespace modules\businesslogic\web\assets;
 
 use craft\web\AssetBundle;
-use misterbk\mix\Mix;
 
 class LayoutAssets extends AssetBundle
 {
@@ -14,19 +13,16 @@ class LayoutAssets extends AssetBundle
     {
         parent::init();
 
-        $mix = Mix::$plugin->mix;
-        $siteUrl = getenv('CONFIG_SITEURL');
-
         $this->css = [
             'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css',
-            $siteUrl.$mix->version('css/styles.css'),
+            '/resources/css/styles.css',
         ];
 
         $this->js = [
             'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js',
-//            $siteUrl.$mix->version('js/manifest.js'),
-//            $siteUrl.$mix->version('js/vendor.js'),
-//            $siteUrl.$mix->version('js/scripts.js'),
+//            '/resources/js/manifest.js',
+//            '/resources/js/vendor.js',
+//            '/resources/js/scripts.js',
         ];
     }
 
